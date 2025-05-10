44d980a20521babd5fc5ca1a4afddd72b3edd644
htikelinn <htikezawlinn@gmail.com>
2025-05-10 23:29:59 +0630

    del Necessary line

--- Exam/ExamData.java
+++ Exam/ExamData.java
@@ -3,9 +3,7 @@ package Exam;
 import java.util.Scanner;
 
 public class ExamData {
-    // String sub;
-    // int tMark;
-    // int studMark;
+
     private String subject; // Moved from local to class-level
     // private int totalMark;
     private int studentMark;
@@ -15,11 +13,6 @@ public class ExamData {
         try (Scanner scanner = new Scanner(System.in)) { // Using try-with-resources to ensure scanner is closed
             System.out.print("Enter Subject : ");
             subject = scanner.nextLine(); // Removed 'private' as it's now a class-level field
-            // subject = scanner.nextLine();
-
-            // System.out.print("Enter Total Mark : ");
-            // totalMark = scanner.nextInt();
-
             System.out.print("Enter Student Mark : ");
             studentMark = scanner.nextInt();
         }
